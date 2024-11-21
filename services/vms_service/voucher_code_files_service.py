063f92f1f8a03cf38c6fe8fd6d970aaab4d8f491
joms <josemari.masangkayv@gmail.com>
2024-11-21 12:59:58 +0800

    Update: refactored code and added error handling for locating file and upload

--- services/vms_service/voucher_code_files_service.py
+++ services/vms_service/voucher_code_files_service.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from util.logger import logger
 import os
+import sys
 from datetime import datetime, timedelta
 import time
 
@@ -10,11 +11,50 @@ class VoucherCodeFileService:
     def __init__(self, wb):
         self.folder_path = os.environ.get("USERPROFILE")+"\Downloads"
         self.target_file_prefix = "vms_vouchercodes"
-        #self.uploaded_filename = "vms_vouchercodes_grabtm.20240821.150000.csv.asc"  # Put filename here
         self.uploaded_filename = ""
         self.today = datetime.now().strftime("%m-%d-%Y")
         self.yesterday = (datetime.now() - timedelta(days=1)).strftime("%m-%d-%Y")
         self.wb = wb
+
+    def locate_file(self):
+        file_path_final = ""
+        logger.info("Locationg file...")
+        for file in os.listdir(self.folder_path):
+            if file.startswith(self.target_file_prefix):
+                #Get the filename
+                self.uploaded_filename = file
+                #Join path with filename
+                file_path_final = os.path.join(self.folder_path, file)
+                return file_path_final
+            
+        if not file_path_final:
+            raise Exception(f"\nError: File '{self.target_file_prefix}' not found in folder '{self.folder_path}'.")
+
+        return file_path_final
+
+    def upload_file_via_browser(self, file_path):
+        try:
+            logger.info("File found! upload in progress..")
+            # Locate the file input element and send the file path directly
+            upload_button = self.wb.chrome_driver.find_element(By.NAME, "file")
+            upload_button.send_keys(file_path)  # Send the file path directly
+            time.sleep(2)
+
+            # Submit the form
+            submit_button = self.wb.chrome_driver.find_element(By.XPATH, "//input[@type='submit']")
+            submit_button.click()
+            time.sleep(3)
+
+            # Wait for the success message to appear
+            success_message = WebDriverWait(self.wb.chrome_driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='alert alert-success' and text()='Succesfully uploaded file.']"))
+            )
+            logger.info("Message found: " + success_message.text)
+            logger.info("File Path: " + file_path)
+        except Exception as e:
+            logger.info("UPLOAD ERROR! make sure filename is in the format vms_vouchercodes_<PARTNER_NAME>.<YYYYMMDD>.<HHMMSS>.csv.asc")
+            print(e)
+            sys.exit(1)
 
     def apply_filter(self):
         logger.info("Applying filter to get vms voucher details")
@@ -39,36 +79,6 @@ class VoucherCodeFileService:
         apply_button = self.wb.chrome_driver.find_element(By.ID, "search3")
         apply_button.click()
 
-    def locate_file(self):
-
-        for file in os.listdir(self.folder_path):
-            if file.startswith(self.target_file_prefix):
-                #Get the filename
-                self.uploaded_filename = file
-                #Join path with filename
-                return os.path.join(self.folder_path, file)
-        return None
-
-    def upload_file_via_browser(self, file_path):
-        
-        # Locate the file input element and send the file path directly
-        upload_button = self.wb.chrome_driver.find_element(By.NAME, "file")
-        upload_button.send_keys(file_path)  # Send the file path directly
-        time.sleep(2)
-
-                # Submit the form
-        submit_button = self.wb.chrome_driver.find_element(By.XPATH, "//input[@type='submit']")
-        submit_button.click()
-        time.sleep(3)
-
-        # Wait for the success message to appear
-        success_message = WebDriverWait(self.wb.chrome_driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='alert alert-success' and text()='Succesfully uploaded file.']"))
-        )
-        print("Message found:", success_message.text)
-        print("File Path: ", file_path)
-
-
     def check_error_code(self):
         
         self.apply_filter()
@@ -89,7 +99,7 @@ class VoucherCodeFileService:
  
         if get_error_code > 0:
             #logger.info("FOUND ERROR CODE, SEE ERROR DESCRIPTION: " + get_error_message)
-            raise Exception("ERROR DESCRIPTION: " + get_error_message)
+            raise Exception("\nERROR CODE: " + str(get_error_code) + "\nERROR DESCRIPTION: " + get_error_message)
             #self.wb.chrome_driver.quit()
             
         else: 
